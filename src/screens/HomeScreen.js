abfc01cad1278d48b18a2ab4bfb4f0f5f7384796
Syransong <ksysong@gmail.com>
2021-06-14 22:01:08 -0700

    create preliminary button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.textStyle}>HomePage</Text>;
+  return (
+    <View>
+      <Text style={styles.textStyle}>HomePage</Text>
+      <Button
+        onPress={() => console.log("Button pressed")}
+        title="Go to Components Demo"
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
