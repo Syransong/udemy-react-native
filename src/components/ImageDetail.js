5ea47b431a84400359ada9ecb3bbfb007e8a345c
Syransong <ksysong@gmail.com>
2021-06-15 22:45:01 -0700

    pass props from imageScreen to imageDetail

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,10 +2,12 @@ import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = (props) => {
-  return <View>
-    <Image source={require("../../assets/beach.jpg")} />
+  return (
+  <View>
+    <Image source={props.imageSource} />
     <Text>{props.title}</Text>
   </View>
+  );
 };
 
 const styles = StyleSheet.create({});
