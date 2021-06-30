010a6aa1ee7486da01f1c5a21121ac4a282cc05e
Syransong <ksysong@gmail.com>
2021-06-29 22:39:46 -0700

    add styling to textInput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
   return <View>
-    <Text>Text Screen</Text>
+    <TextInput style={StyleSheet.input} />
   </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
 
 export default TextScreen;
